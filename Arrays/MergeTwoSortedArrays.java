ae599fc4e45b1ae83ff0296d2f895e05fa049b87
Manoj0305-student <128007896+Manoj0305-student@users.noreply.github.com>
2026-06-03 14:08:12 +0530

    Added array problem and OOPs concept floder.

--- Arrays/MergeTwoSortedArrays.java
+++ Arrays/MergeTwoSortedArrays.java
@@ -30,8 +30,8 @@ public class MergeTwoSortedArrays {
 }
 
 public static void main(String[] args) {
-  int arr1[] = {1, 3, 5, 7};
-  int arr2[] = {2, 4, 6, 8};
+  int arr1[] = {1, 4, 7, 9};
+  int arr2[] = {2, 3, 5, 6};
   mergeArraysWithoutAnExtraSpace(arr1, arr2);
 }
   
---
+++ OOPs/Encapsulation.java
@@ -0,0 +1,49 @@
+package OOPs;
+
+class BankAccount {
+
+  private double balance = 1000.0;
+
+  public boolean withdraw(double amount) {
+
+    if(amount <= 0) {
+      System.out.println("Amount cannot be negative");
+      return false;
+    }
+
+    if(amount > balance) {
+      System.out.println("Insuffiecent Balance");
+      return false;
+    }
+
+    balance -= amount;
+    return true;
+  }
+
+  public void deposit(double amount) {
+    if(amount <= 0) {
+      System.out.println("Cannot deposit this amount. Please check the amount");
+    }
+    balance += amount;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+}
+
+public class Encapsulation {
+
+  // Hiding the data, showing only reqiured data to the user.
+
+  public static void main(String[] args) {
+
+   BankAccount bank = new BankAccount();
+   bank.deposit(500);
+   bank.withdraw(200);
+   double result = bank.getBalance();
+   System.out.println("Remainig Balance: "+result);
+
+  }
+  
+}
